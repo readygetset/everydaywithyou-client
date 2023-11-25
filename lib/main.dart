1facef0aeaec5b65ce957d786b0ec0d41567d659
readygetset <ksp7292@gmail.com>
2023-11-26 00:15:19 +0900

    ftr: add mainpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'questionList.dart';
+import 'todayQuestion.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: questionListWidget()
+      home: todayQuestionWidget()
     );
   }
 }
